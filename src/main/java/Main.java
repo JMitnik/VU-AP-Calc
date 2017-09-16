cb6273e9d5b758a6d66a9e9baed22d47b187206b
Jonathan Mitnik <JMitnik@gmail.com>
2017-09-16 23:06:16 +0200

    ˆ

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,9 +23,11 @@ public class Main implements CalculatorInterface {
     }
 
     private void start() {
+
     }
 
     public static void main(String[] argv) {
         new Main().start();
     }
 }
+
